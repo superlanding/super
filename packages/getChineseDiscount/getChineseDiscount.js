1ee1ea04d88600d118215b6475c3a8bf8bb1fad1
kmsheng <kmsh3ng@gmail.com>
2020-05-27 13:17:10 +0800

    update getChineseDiscount method

--- packages/getChineseDiscount/getChineseDiscount.js
+++ packages/getChineseDiscount/getChineseDiscount.js
@@ -1,6 +1,10 @@
+// >= 1 -> '' 沒折扣就不顯示
 // 0.75 -> 75 (折)
 // 0.7 -> 7 (折)
 export default function getChineseDiscount(rate) {
+  if (rate >= 1) {
+    return ''
+  }
   const num = parseInt(rate * 100, 10)
   return parseInt(String(num).replace(/0+$/, ''), 10)
 }
