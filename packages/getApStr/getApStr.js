a73e0dbceaaee895e56ff6eb4d0f90f731f71cec
kmsheng <kmsh3ng@gmail.com>
2020-07-10 16:08:02 +0800

    add comments to getApStr

--- packages/getApStr/getApStr.js
+++ packages/getApStr/getApStr.js
@@ -1,6 +1,10 @@
 import range from 'lodash.range'
 
 // https://en.wikipedia.org/wiki/Arithmetic_progression
+//
+// qty: initial base value
+// interval: number to add each time
+// n: how many times to add
 export default function getApStr({ qty, interval, n, separator }) {
   return range(0, (n || 4)).map(i => qty + ((i + 1) * interval))
     .join(separator || ', ')
