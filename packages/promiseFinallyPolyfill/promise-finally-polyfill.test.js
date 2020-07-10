fd60e2fa5e5244bb54475042a2c1cd7edc10630c
kmsheng <kmsh3ng@gmail.com>
2020-07-10 16:13:07 +0800

    update promise-finally-polyfill.test.js

--- packages/promiseFinallyPolyfill/promise-finally-polyfill.test.js
+++ packages/promiseFinallyPolyfill/promise-finally-polyfill.test.js
@@ -9,4 +9,23 @@ test('promise-finally-polyfill', t => {
 
   // shouldn't be native
   t.true(! Promise.prototype.finally.toString().includes('native code'))
+
+  const doSomething = (success) => {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        return resolve()
+      }
+      reject()
+    })
+  }
+
+  let calledThen = false
+
+  doSomething(true)
+    .then(() => {
+      calledThen = true
+    })
+    .finally(() => {
+      t.true(calledThen)
+    })
 })
