ea8a8a9cc4bac3fa7876a362946df42b3a09e619
kmsheng <kmsh3ng@gmail.com>
2020-07-10 17:07:33 +0800

    update toPromise.test.js

--- packages/toPromise/toPromise.test.js
+++ packages/toPromise/toPromise.test.js
@@ -1,27 +1,112 @@
 import { test } from '../../test/utils'
-import toPromise from './toPromise'
+import toPromise, { clearTimer } from './toPromise'
 
-test('toPromise', t => {
+const promiseTimeout = (fn, duration) => {
+  let timer
+  const promise = new Promise((resolve, reject) => {
+    timer = setTimeout(() => {
+      fn()
+      resolve()
+    }, duration)
+  })
+  return { promise, timer }
+}
+
+test('clearTimer', async t => {
+  const { timer, promise } = promiseTimeout(() => t.fail(), 0)
+  const config = { timer }
+  clearTimer(config)
+  const res = promiseTimeout(() => t.pass(), 300)
+  await res.promise
+})
+
+test('toPromise: regular action', t => {
+  const store = {
+    dispatch() {
+    }
+  }
+  const action = {
+    type: 'TEST',
+  }
+  const next = a => t.deepEqual(a, action)
+  toPromise(store)(next)(action)
+})
+
+test('toPromise: async action', t => {
   const store = {
     dispatch() {
     }
   }
   const next = () => {}
-
   const asyncAction = {
-    type: 'test action',
+    type: 'TEST',
     async: {
-      resolve: 'success',
-      reject: 'fail'
+      resolve: 'SUCCESS',
+      reject: 'FAIL'
     }
   }
-  const res1 = toPromise(store)(next)(asyncAction)
+  const res = toPromise(store)(next)(asyncAction)
+  t.true(res instanceof Promise)
+})
 
-  t.true(res1 instanceof Promise)
+test('toPromise: async action with loading', t => {
+  return new Promise(resolve => {
 
-  const action = {
-    type: 'test action',
-  }
-  const res2 = toPromise(store)(next)(action)
-  t.false(res2 instanceof Promise)
+    const next = () => {}
+    const asyncAction = {
+      _id: 'secret',
+      type: 'TEST',
+      async: {
+        resolve: 'SUCCESS',
+        reject: 'FAIL',
+        loading: 'LOADING'
+      }
+    }
+    const store = {
+      dispatch(action) {
+        t.is(action.type,  asyncAction.async.loading)
+      }
+    }
+    const nextFunc = toPromise(store)(next)
+    const p = nextFunc(asyncAction)
+    t.true(p instanceof Promise)
+
+    setTimeout(() => {
+      nextFunc({
+        _id: asyncAction._id,
+        type: asyncAction.async.resolve
+      })
+    }, 1000)
+
+    p.then(() => resolve())
+  })
+})
+
+test('toPromise: reject async action', t => {
+  return new Promise(resolve => {
+
+    const next = () => {}
+    const asyncAction = {
+      _id: 'secret',
+      type: 'TEST',
+      async: {
+        resolve: 'SUCCESS',
+        reject: 'FAIL'
+      }
+    }
+    const store = {
+      dispatch(action) {
+      }
+    }
+    const nextFunc = toPromise(store)(next)
+    const p = nextFunc(asyncAction)
+    t.true(p instanceof Promise)
+
+    nextFunc({
+      _id: asyncAction._id,
+      type: asyncAction.async.reject
+    })
+
+    p.catch(() => resolve())
+  })
 })
