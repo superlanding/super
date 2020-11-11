51195af65756a1d18de1d6e90525bf21c84391c5
kmsheng <kmsh3ng@gmail.com>
2020-11-11 11:37:45 +0800

    update dist/getApStr

--- packages/getApStr/dist/getApStr.js
+++ packages/getApStr/dist/getApStr.js
@@ -10,6 +10,10 @@ var _lodash = _interopRequireDefault(require("lodash.range"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // https://en.wikipedia.org/wiki/Arithmetic_progression
+//
+// qty: initial base value
+// interval: number to add each time
+// n: how many times to add
 function getApStr(_ref) {
   var qty = _ref.qty,
       interval = _ref.interval,
