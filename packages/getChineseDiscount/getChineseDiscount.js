cb360ab4c9deadddb4d1b7da189569487a74fe1d
kmsheng <kmsh3ng@gmail.com>
2021-07-27 15:04:30 +0800

    fix getChineseDiscount.js

--- packages/getChineseDiscount/getChineseDiscount.js
+++ packages/getChineseDiscount/getChineseDiscount.js
@@ -1,10 +1,8 @@
 // >= 1 -> '' 沒折扣就不顯示
 // 0.75 -> 75 (折)
 // 0.7 -> 7 (折)
+// 0.57 -> 57 (折)
 export default function getChineseDiscount(rate) {
-  if (rate >= 1) {
-    return ''
-  }
-  const num = parseInt(rate * 100, 10)
-  return parseInt(String(num).replace(/0+$/, ''), 10)
+  const [, discount] = String(rate).match(/^0.(\d+)$/) || []
+  return discount || ''
 }
--- packages/getChineseDiscount/getChineseDiscount.test.js
+++ packages/getChineseDiscount/getChineseDiscount.test.js
@@ -3,6 +3,8 @@ import getChineseDiscount from './getChineseDiscount'
 
 test('getChineseDiscount', t => {
   t.is(getChineseDiscount(1), '')
-  t.is(getChineseDiscount(0.75), 75)
-  t.is(getChineseDiscount(0.7), 7)
+  t.is(getChineseDiscount(0.75), '75')
+  t.is(getChineseDiscount(0.7), '7')
+  t.is(getChineseDiscount(0.57), '57')
+  t.is(getChineseDiscount(0.572), '572')
 })
