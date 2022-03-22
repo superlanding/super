0c429109e84e804755527fa5ec221547d7eaa403
kmsheng <kmsh3ng@gmail.com>
2022-03-22 14:34:17 +0800

    update dist/commaSeparate.js

--- packages/commaSeparate/dist/commaSeparate.js
+++ packages/commaSeparate/dist/commaSeparate.js
@@ -5,7 +5,36 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = commaSeparate;
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 function commaSeparate(num) {
   var values = String(num).split('.');
-  return values[0].replace(/.(?=(?:.{3})+$)/g, '$&,') + (values.length == 2 ? '.' + values[1] : '');
+
+  var _values = _slicedToArray(values, 2),
+      whole = _values[0],
+      decimal = _values[1];
+
+  var negative = whole.startsWith('-');
+  var hasDot = values.length === 2;
+  var formattedWhole = negative ? '-' + addComma(whole.slice(1)) : addComma(whole);
+
+  if (hasDot) {
+    return formattedWhole + '.' + decimal;
+  }
+
+  return formattedWhole;
+}
+
+function addComma(str) {
+  return str.replace(/.(?=(?:.{3})+$)/g, '$&,');
 }
