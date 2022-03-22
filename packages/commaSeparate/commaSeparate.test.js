39c7ab20e8d0e5fde3691874b8357095d809302e
kmsheng <kmsh3ng@gmail.com>
2022-03-22 14:33:37 +0800

    add negative and negative decimal test to commaSeparate.test.js

--- packages/commaSeparate/commaSeparate.test.js
+++ packages/commaSeparate/commaSeparate.test.js
@@ -8,8 +8,14 @@ test('commaSeparate', t => {
   t.is(commaSeparate(1234), '1,234')
   t.is(commaSeparate(1234567), '1,234,567')
 
+  // negative
+  t.is(commaSeparate(-100), '-100')
+
   // decimal point
   t.is(commaSeparate(0.123), '0.123')
   t.is(commaSeparate(1000.123), '1,000.123')
   t.is(commaSeparate(123456789.1234567), '123,456,789.1234567')
+
+  // negative with decimal point
+  t.is(commaSeparate(-12345), '-12,345')
 })
